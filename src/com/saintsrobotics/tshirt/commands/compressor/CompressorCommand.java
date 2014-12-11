2dca30192ade5065c30bf8462ac159f05b418ae6
SR <SR@SR-PC>
2014-12-10 22:17:58 -0800

    CompressorCommand finished.

--- src/com/saintsrobotics/tshirt/commands/compressor/CompressorCommand.java
+++ src/com/saintsrobotics/tshirt/commands/compressor/CompressorCommand.java
@@ -5,9 +5,17 @@ import com.saintsrobotics.tshirt.commands.CommandBase;
 public class CompressorCommand extends CommandBase {
 
     protected void initialize() {
+        requires(compressorSubsystem);
     }
 
     protected void execute() {
+        if(compressorSubsystem.getPressureSwitch() && !compressorSubsystem.getCompressor()) {  //If Pressure is less than 115 psi and compressor is off--this may need to be changed based on orientation of pressure switch
+                compressorSubsystem.setCompressor(true);   //Turn on compressor
+        }
+        
+        else if (compressorSubsystem.getPressureSwitch() && compressorSubsystem.getCompressor()){
+            compressorSubsystem.setCompressor(false);  //If pressure is already 115 and compressor in on, turn off compressor.
+        }
     }
 
     protected boolean isFinished() {
